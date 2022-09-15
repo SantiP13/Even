9a04abd42dc8d8ae39917629296440c5395cbfd6
Santiago Peñúñuri <s.penunuri@hotmail.com>
2022-09-15 10:54:39 -0500

    Home Layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styled from "styled-components";
 
+import BG2 from "../dist/bg2.png";
+
 export default function Home() {
+  const [where, setWhere] = useState(false);
+
+  const handleWhat = (e) => {};
+
   return (
     <Container>
       <LogoContainer>LOGO</LogoContainer>
-      <SubContainer></SubContainer>
+      <SubContainer>
+        <IMG src={BG2} />
+        <SearchContainer>
+          <Title>Encuentra la empresa que buscas</Title>
+          <Text>Ayúdanos opinando al respecto</Text>
+
+          <Input
+            placeholder="¿Qué estás buscando?"
+            onChange={handleWhat}
+          ></Input>
+          {where ? (
+            <Input placeholder="¿Algún lugar en mente?"></Input>
+          ) : (
+            <div></div>
+          )}
+        </SearchContainer>
+      </SubContainer>
     </Container>
   );
 }
 
 const Container = styled.div`
-  height: 100%;
-
-  background-color: cyan;
+  height: 90%;
 `;
 
 const LogoContainer = styled.div`
@@ -39,8 +59,54 @@ const LogoContainer = styled.div`
 const SubContainer = styled.div`
   display: flex;
 
-  background-color: gray;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: transparent;
 
   width: 100%;
   height: 100%;
+`;
+
+const IMG = styled.img`
+  height: 80%;
+
+  opacity: 80%;
+
+  margin: 0 2%;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 50%;
+  height: inherit;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.label`
+  margin: 1% 0;
+
+  font-size: 2.4rem;
+`;
+
+const Text = styled.label`
+  margin: 1% 0;
+
+  font-size: 1.2rem;
+  font-weight: 400;
+`;
+
+const Input = styled.input`
+  margin: 2% 0;
+  padding: 1% 2%;
+
+  height: 5%;
+  width: 60%;
+
+  border-radius: 3px;
+  border-color: #000;
 `;
